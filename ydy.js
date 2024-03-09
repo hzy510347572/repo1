671bab25c05b265853be96e054f65d1a1e276e74
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-09 08:45:44 +0800

    Update ydy.js

--- ydy.js
+++ ydy.js
@@ -1,12 +1,14 @@
 
 var objc = JSON.parse($response.body);
 
-objc.svip=1910086400000
+objc.svip=2
 objc.end=1910086400000
 objc.payYear=true
 objc.userType=2
 objc.service=2
 objc.student=true
+objc.remaining=900000
+objc.ituneRemaining=800000
 
 console.log(JSON.stringify(objc))
 
