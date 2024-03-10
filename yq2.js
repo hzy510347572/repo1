0663ffc1af07dde7764bf6d8f1dad975bcc450b7
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 07:01:48 +0800

    Update yq2.js

--- yq2.js
+++ yq2.js
@@ -6,8 +6,10 @@ var obj = JSON.parse($response.body);
 obj.body.vipZoneInfo.isSVipUser=true;
 obj.body.vipZoneInfo.isVipUser=true;
 
-obj.body.priceInfo.isFree=true;
+obj.body.bookInfo.priceInfo.isFree=true;
 obj.body.vipBuyBookInfo.isVipUser=1;
 obj.body.vipBuyBookInfo.isVipUser=19111111111;
+obj.body.vipBuyBookInfo.vBuy=1;
+
 
 $done({body : JSON.stringify(obj)});
