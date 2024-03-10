949679cc976954123a948d81704a31d443c4b75f
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 07:16:01 +0800

    Update yq3.js

--- yq3.js
+++ yq3.js
@@ -5,4 +5,13 @@ var obj = JSON.parse($response.body);
 
 obj.body.user.is_vip=true;
 
+obj.body.user.coupon_amount=99999999;
+obj.body.user.total_amount=99999999;
+obj.body.user.account_amount=99999999;
+obj.body.pay.amount=0;
+obj.body.pay.gap_yb_amount=0;
+obj.body.pay.is_enough=true;
+
+
+
 $done({body : JSON.stringify(obj)});
