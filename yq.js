33420b67e2e7c7708d61fd8c43a1711ccd2f7c07
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 02:04:28 +0800

    Create yq.js

---
+++ yq.js
@@ -0,0 +1,16 @@
+
+var obj = JSON.parse($response.body);
+
+obj.data.me.member_level=2;
+//obj.data.me.memberLevelName="超级会员";
+obj.data.me.isTopLevel=true;
+obj.data.me.expired_at="2099-11-29T15:59:59.000Z";
+obj.data.interest.owner.member_level=2;
+obj.data.interest.owner.isTopLevel=true;
+obj.data.interest.owner.isMemberTopLevel=true;
+obj.data.interest.owner.isExpired=false;
+obj.data.interest.interests.open_api=true;
+obj.data.interest.userMemberInfo.expired_at="2099-11-29T15:59:59.000Z";
+
+console.log(JSON.stringify(obj))
+$done({body : JSON.stringify(obj)});
