7e29c421c12e9bd4343523ddf2d9bc21c7f5952b
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 05:10:16 +0800

    Update yq1.js

--- yq1.js
+++ yq1.js
@@ -1,8 +1,11 @@
 var obj = JSON.parse($response.body);
 
-obj.body.userInfo.isSvip=1;
-obj.body.userInfo.isvip=1;
-obj.body.zyVip.isvip=1;
+obj.body.userInfo.isSvip=true;
+obj.body.userInfo.isVip=true;
+obj.body.userInfo.isUpgrade=true;
+obj.body.userInfo.verification=true;
+
+obj.body.zyVip.isVip=1;
 obj.body.vipGuide.vipStatus=1;
 obj.body.vipGuide.svipStatus=1;
 
