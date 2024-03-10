0ff9a072d291c908649918c06f3c1ba815559cb1
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 03:39:47 +0800

    Update yq1.js

--- yq1.js
+++ yq1.js
@@ -1,11 +1,8 @@
 var obj = JSON.parse($response.body);
 
-obj.data.user.member_level=3;
-obj.data.user.memberLevelName="超级会员";
-obj.data.user.role=2;
-obj.data.user.isPaid=true;
-obj.data.user.isTopLevel=true;
-obj.data.user.expired_at="2099-11-29T15:59:59.000Z";
+obj.data.order.amount=0.00;
+obj.data.order.product_amount="0.00";
+
 
 
 console.log(JSON.stringify(obj))
