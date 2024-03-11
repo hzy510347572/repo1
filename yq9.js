3e9cb0d4f29e8f65345991cfba060cbf3bb1b8cf
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 15:00:52 +0800

    Update yq9.js

--- yq9.js
+++ yq9.js
@@ -3,6 +3,7 @@ var obj = JSON.parse($response.body);
 
 obj.body.Data.Price=0;
 obj.body.Data.Type=2;
+obj.body.Data.Url=obj.body.Data.Url.replace(/price=[1-9]\d*\.?\d*)|(0\.\d*[1-9]/g, "price=0");
 
 console.log(JSON.stringify(obj))
 $done({body : JSON.stringify(obj)});
