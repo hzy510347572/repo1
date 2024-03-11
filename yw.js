b7fe262f1d709a544eae14a05874167061a5fbdf
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 15:58:34 +0800

    Update yw.js

--- yw.js
+++ yw.js
@@ -2,10 +2,12 @@
 var obj = JSON.parse($response.body);
 
 for (i = 0; i < obj.body.list.length; i++) { 
-  for (j = 0; j < obj.body.list[i].items.length; j++) { 
-      obj.body.list[i].items[j].isVipUser = true;
-
+   if(obj.body.list[i].items != null && obj.body.list[i].items != "undefined"){
+       for (j = 0; j < obj.body.list[i].items.length; j++) { 
+        obj.body.list[i].items[j].isVipUser = true;
+       }
    }
+ 
     
  }
 
