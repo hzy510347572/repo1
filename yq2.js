35a0352a51d130ba78a6ca6bb720c3d3b21fb3c8
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 18:14:38 +0800

    Update yq2.js

--- yq2.js
+++ yq2.js
@@ -9,7 +9,12 @@ obj.body.vipZoneInfo.isVipUser=true;
 obj.body.bookInfo.priceInfo.isFree=true;
 obj.body.vipBuyBookInfo.isVipUser=1;
 obj.body.vipBuyBookInfo.vipExpiretime=19111111111;
-obj.body.vipBuyBookInfo.vBuy=1;
+//obj.body.vipBuyBookInfo.vBuy=1;
+
+if(obj.body.buttonInfo != null){
+    obj.body.buttonInfo[0].cmd.Data.DonloadInfo.bookCatalog.downloadType = 2;
+    obj.body.buttonInfo[0].cmd.Data.DonloadInfo.DownloadType = 2;
+}
 
 
 $done({body : JSON.stringify(obj)});
