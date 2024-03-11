fbd201496283fee03bd9eb5b0e0520a4d7c74936
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 18:47:25 +0800

    Update yw5.js

--- yw5.js
+++ yw5.js
@@ -1,1 +1,12 @@
 
+
+var obj = JSON.parse($response.body);
+
+obj.body.amount.voucherAmount=99999999;
+obj.body.amount.voucherAmountExpire=19999999999;
+obj.body.amount.totalAmount=999999999;
+
+
+
+console.log(JSON.stringify(obj))
+$done({body : JSON.stringify(obj)});
