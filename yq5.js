590d542c1e51e4dc82ff47bf2f57e3cca10c8d8b
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 12:05:07 +0800

    Update and rename yq6.js to yq5.js

--- yq5.js
+++ yq5.js
@@ -3,7 +3,7 @@ var obj = JSON.parse($response.body);
 
 obj.body.Data.Charging.Price="0";
 obj.body.tplInfo.total=9999999;
-obj.body.tplInfo.button.url=obj.body.tplInfo.button.url.replace(/price=/g, "price=-")");
+obj.body.tplInfo.button.url=obj.body.tplInfo.button.url.replace("price=", "price=-");
 
 
 
