a799ba5b0e8ad497e37ffa2610c214fc036e86ae
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 14:43:41 +0800

    Update yq7.js

--- yq7.js
+++ yq7.js
@@ -6,7 +6,8 @@ var obj = JSON.parse($response.body);
 
 obj.tplInfo.amount="99999999阅饼";
 obj.tplInfo.total=99999999;
-obj.tplInfo.price="0阅饼"
+obj.tplInfo.price="0阅饼";
+obj.tplInfo.button[0].url=obj.tplInfo.button[0].url.replace(/price=[0-9]/g, "price=0");
 
 
 
