8149d38c0f2ba718ee99bad2c81a89cd9cc32550
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 21:41:15 +0800

    Update yq7.js

--- yq7.js
+++ yq7.js
@@ -3,6 +3,17 @@
 var obj = JSON.parse($response.body);
 
 
+obj.Charging.FeeType=2;
+
+obj.bookCatalog.isEpubSerialize="1";
+obj.bookCatalog.downloadType="1";
+obj.bookCatalog.type="0";
+
+// obj.bookCatalog.relBookId="12747058";
+// obj.relResource.book_id="12747058";
+obj.relResource.status="10";
+obj.relResource.type="0";
+
 
 if(obj.tplInfo != null && obj.tplInfo != "undefined"){
     obj.tplInfo.amount="99999999阅饼";
