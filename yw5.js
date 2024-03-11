44c74c85cc7e3beddf6183fdc478e936c10f78ea
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 17:56:20 +0800

    Update yw5.js

--- yw5.js
+++ yw5.js
@@ -1,12 +1,1 @@
-var str = $response.body;
 
-str.replaceAll(/\&orderPrice\=463\.30/g, "\&orderPrice\=0");
-str.replaceAll(/data-js\=\"btn\"/g, "data-js\=\"vip-discount\"");
-
-str.replace(/class\=\"pay_wrap new_log_wrap\" data\-gap\=\"储值订单\" data\-price\=\"46330阅饼\" data\-oriPrice\=\"46330阅饼\" data\-prePrice\=\"none\" data\-gapPrice\=\"463.30元\" data\-gapPriceIos\=\"none\" data\-userType\=\"会员书\" data\-feeUnit\=\"按本\" data\-account\-amount\=\"0.00\" data\-coupon\-amount\=\"0\"/g, "class=\"pay_wrap new_log_wrap\" data-gap=\"储值订单\" data-price=\"0阅饼\" data-oriPrice=\"0阅饼\" data-prePrice=\"none\" data-gapPrice=\"0元\" data-gapPriceIos=\"none\" data-userType=\"会员书\" data-feeUnit=\"按本\" data-account-amount=\"99999999.00\" data-coupon-amount=\"99999999\"");
-
-console.log("=====================html替换===============");
-console.log(str);
-console.log("=====================html替换===============");
-
-$done({body : str});
