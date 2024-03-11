4d4a2380e908736bb8d3d8d6c41c147c9d017267
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 17:32:58 +0800

    Update and rename yw4.js to yw5.js

--- yw5.js
+++ yw5.js
@@ -3,6 +3,6 @@ var str = $response.body;
 str.replaceAll("\&orderPrice\=463\.30", "\&orderPrice\=0");
 str.replaceAll("data-js\=\"btn\"", "data-js\=\"left-btn\"");
 
-console.log("=====================html替换===============")
+console.log("=====================html替换===============");
 
-$done({body : $response.body});
+$done({body : str});
