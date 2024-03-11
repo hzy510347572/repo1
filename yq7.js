440034e1972956f6ebeeba37bc1dc1c629adba5c
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 16:02:18 +0800

    Update yq7.js

--- yq7.js
+++ yq7.js
@@ -3,12 +3,12 @@
 var obj = JSON.parse($response.body);
 
 
-
-obj.tplInfo.amount="99999999阅饼";
-obj.tplInfo.total=99999999;
-obj.tplInfo.price="0阅饼";
-obj.tplInfo.button[0].url=obj.tplInfo.button[0].url.replaceAll(/price\=\d+/g, "price=0");
-
+if(obj.tplInfo != null && obj.tplInfo != "undefined"){
+    obj.tplInfo.amount="99999999阅饼";
+    obj.tplInfo.total=99999999;
+    obj.tplInfo.price="0阅饼";
+    obj.tplInfo.button[0].url=obj.tplInfo.button[0].url.replaceAll(/price\=\d+/g, "price=0");
+}
 
 console.log(JSON.stringify(obj));
 
