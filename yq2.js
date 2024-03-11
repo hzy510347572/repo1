c0e2c0af60f99b8bafb3c8d55b8d5d7f3ea59c17
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 12:22:55 +0800

    Update yq2.js

--- yq2.js
+++ yq2.js
@@ -8,7 +8,7 @@ obj.body.vipZoneInfo.isVipUser=true;
 
 obj.body.bookInfo.priceInfo.isFree=true;
 obj.body.vipBuyBookInfo.isVipUser=1;
-obj.body.vipBuyBookInfo.isVipUser=19111111111;
+obj.body.vipBuyBookInfo.vipExpiretime=19111111111;
 obj.body.vipBuyBookInfo.vBuy=1;
 
 
