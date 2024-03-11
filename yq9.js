2a717158efa6f0587d988bb6960e7d2ecc027276
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-11 15:31:40 +0800

    Update yq9.js

--- yq9.js
+++ yq9.js
@@ -4,6 +4,7 @@ var obj = JSON.parse($response.body);
 obj.body.Data.Price=0;
 obj.body.Data.Type=2;
 obj.body.Data.Url=obj.body.Data.Url.replaceAll(/price\=\d+\.\d+/g, "price=0");
+obj.body.Data.OrderUrl=obj.body.Data.OrderUrl.replaceAll(/price\=\d+\.\d+/g, "price=0");
 
 console.log(JSON.stringify(obj))
 $done({body : JSON.stringify(obj)});
