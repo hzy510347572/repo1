1aa682a881318a398f0427a4a1b33bc7f2037d87
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-12 12:03:19 +0800

    Update zh7.js

--- wind-fire-thunder-lightning/script/zhihu/zh7.js
+++ wind-fire-thunder-lightning/script/zhihu/zh7.js
@@ -5,7 +5,7 @@ var obj = JSON.parse($response.body);
 for (i = 0; i < obj.data.length; i++) { 
     obj.data[i].is_locked=false;
     obj.data[i].right.has_ownership=true;
-    // obj.data[i].is_free=true;
+    obj.data[i].is_free=true;
    
  }
 console.log("---------" + obj.data[0].is_locked + "---------");
