c41e1a60c20d1380d1a1848ce6db6e6d7a3300ca
hzy <hzy510347572@163.com>
2024-03-12 12:31:47 +0800

    R1 msg:提交zh7

--- wind-fire-thunder-lightning/script/zhihu/zh7.js
+++ wind-fire-thunder-lightning/script/zhihu/zh7.js
@@ -1,13 +1,17 @@
 
 var obj = JSON.parse($response.body);
 
+if (obj.data != null) {
+    for (i = 0; i < obj.data.length; i++) {
+        obj.data[i].is_locked=false;
+        obj.data[i].right.has_ownership=true;
+        obj.data[i].is_free=true;
 
-for (i = 0; i < obj.data.length; i++) { 
-    obj.data[i].is_locked=false;
-    obj.data[i].right.has_ownership=true;
-    obj.data[i].is_free=true;
-   
- }
-console.log("---------" + obj.data[0].is_locked + "---------");
+    }
+    console.log("---------" + obj.data[0].is_locked + "---------");
+}else {
+    console.log(obj);
+}
+
 
 $done({body : JSON.stringify(obj)});
