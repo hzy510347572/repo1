4514f2669bc320543d4892da6c7c67e5733bb961
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 00:05:46 +0800

    Update yw2.js

--- yw2.js
+++ yw2.js
@@ -7,7 +7,7 @@ obj.type=0;
 obj.freeType=3;
 obj.status=0;
 obj.vipCode="vip";
-obj.body.tpken="cl/gggWXy+sciRJr/4L9KoqHekW8SiMVE80sb+0GMMgYyRGjG56zcS8q76JHd/Lk7o4YOq9OboGOa9NVCQlg6W4FdA1bdVJEIwL2fytzy+keTHQ+f14CkCpJPHX3hj1Rv6FaYiJKtsV+kXJ0Q3Z3b+r+nt08o3UpNhpXt5ybWOA=";
+obj.body.token="cl/gggWXy+sciRJr/4L9KoqHekW8SiMVE80sb+0GMMgYyRGjG56zcS8q76JHd/Lk7o4YOq9OboGOa9NVCQlg6W4FdA1bdVJEIwL2fytzy+keTHQ+f14CkCpJPHX3hj1Rv6FaYiJKtsV+kXJ0Q3Z3b+r+nt08o3UpNhpXt5ybWOA=";
 
 
 
