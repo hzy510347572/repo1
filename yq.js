213db82b6a6b6a3810f213a2d6aab6320594983b
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-12 21:11:07 +0800

    Update yq.js

--- yq.js
+++ yq.js
@@ -4,9 +4,12 @@ var obj = JSON.parse($response.body);
 
 obj.body.VipInfo.is_vip=true;
 obj.body.VipInfo.is_svip=true;
+obj.body.VipInfo.last_buy_vip_time=1700105401;
 obj.body.VipInfo.svip_expire_time=1910105401609;
 obj.body.VipInfo.vip_expire_time=1910105401609;
 
+obj.body.VipInfo.auto_status_type="1";
+obj.body.VipInfo.auto_status="1";
 
 console.log(JSON.stringify(obj))
 $done({body : JSON.stringify(obj)});
