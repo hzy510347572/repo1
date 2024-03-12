f20d14ae9711388846b4c9d99cd8b1c0f7b6b2d7
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-12 19:15:14 +0800

    Update yw3.js

--- yw3.js
+++ yw3.js
@@ -1,8 +1,11 @@
-var obj = response.body;
+
+
+var obj = JSON.parse($response.body);
 
 obj.body.userInfo.is_vip_user=1;
 
 
 
-console.log(obj)
-$done({body : obj});
+console.log(JSON.stringify(obj));
+
+$done({body : JSON.stringify(obj)});
