40baab7de65bdd518601f402efdca29cef8ba476
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 02:05:44 +0800

    Update yw3.js

--- yw3.js
+++ yw3.js
@@ -3,6 +3,11 @@
 var obj = JSON.parse($response.body);
 
 obj.body.userInfo.is_vip_user=1;
+obj.body.list[0].base_price=0;
+obj.body.list[0].discount_price=0;
+obj.body.list[0].is_free=1;
+obj.body.list[0].discount_end_time=19999999999;
+
 
 
 
