6f6359bec70c2120baadf511570090a3b84309ba
hzy <hzy510347572@163.com>
2024-03-12 14:12:56 +0800

    R1 msg:提交zh

--- wind-fire-thunder-lightning/script/zhihu/zh.js
+++ wind-fire-thunder-lightning/script/zhihu/zh.js
@@ -1,9 +1,11 @@
 
 var obj = JSON.parse($response.body);
 
+obj.button.right_icon_text = "";
 obj.button.jump_url = "https://www.zhihu.com/xen/market/vip-privileges";
 obj.button.right_icon_style = 0;
 obj.button.button_style = 0;
+obj.button.sub_text = "";
 obj.button.text = "立即续费";
 
 obj.vip_type = "svip";
@@ -16,6 +18,7 @@ obj.vip_icon.night_url = "https://picx.zhimg.com/v2-e678bd58e61fd9fbba1caaea9818
 
 obj.is_vip_new = false;
 
+obj.show_red_point = false;
 obj.red_point_time = 1710168204;
 obj.is_svip_new = false;
 
