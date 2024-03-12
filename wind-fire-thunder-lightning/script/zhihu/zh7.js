508d0dad2fc0af550b771e3ac70f1f16ea129dce
hzy <hzy510347572@163.com>
2024-03-12 12:35:47 +0800

    R1 msg:提交zh7

--- wind-fire-thunder-lightning/script/zhihu/zh7.js
+++ wind-fire-thunder-lightning/script/zhihu/zh7.js
@@ -4,8 +4,11 @@ var obj = JSON.parse($response.body);
 if (obj.data != null) {
     for (i = 0; i < obj.data.length; i++) {
         obj.data[i].is_locked=false;
-        obj.data[i].right.has_ownership=true;
         obj.data[i].is_free=true;
+        if (obj.data[i].right == null) {
+            obj.data[i].right = {};
+            obj.data[i].right.has_ownership=true;
+        }
 
     }
     console.log("---------" + obj.data[0].is_locked + "---------");
