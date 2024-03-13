d35cbf661e58513975bcb369be18c2fc979bc01e
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 12:14:25 +0800

    Update yw2.js

--- yw2.js
+++ yw2.js
@@ -2,11 +2,12 @@
 var obj = JSON.parse($response.body);
 
 obj.code=0;
-obj.msg="success";
+obj.msg="ok";
 obj.type=0;
-obj.freeType=3;
+obj.freeType=2;
 obj.status=0;
-obj.vipCode="vip";
+obj.vipCode=4;
+obj.type=5;
 obj.body={}
 obj.body.token="cl/gggWXy+sciRJr/4L9KoqHekW8SiMVE80sb+0GMMgYyRGjG56zcS8q76JHd/Lk7o4YOq9OboGOa9NVCQlg6W4FdA1bdVJEIwL2fytzy+keTHQ+f14CkCpJPHX3hj1Rv6FaYiJKtsV+kXJ0Q3Z3b+r+nt08o3UpNhpXt5ybWOA=";
 
