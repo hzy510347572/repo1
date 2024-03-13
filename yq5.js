6b7f8e58b4d1780ed77374a002e27230dc06c693
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 12:08:22 +0800

    Update yq5.js

--- yq5.js
+++ yq5.js
@@ -8,8 +8,8 @@ obj.body.user.is_vip=true;
 obj.body.user.coupon_amount=99999999;
 obj.body.user.total_amount=99999999;
 obj.body.user.account_amount=99999999;
-obj.body.pay.amount=0;
-obj.body.pay.gap_yb_amount=0;
+obj.body.pay.amount=1;
+obj.body.pay.gap_yb_amount=1;
 obj.body.pay.is_enough=true;
 
 obj.body.max_count=99999;
