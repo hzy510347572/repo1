d980fe2d9928e6e8bc5f31f3d4a7d8bbe6b6ab79
hzy <hzy510347572@163.com>
2024-03-13 18:56:12 +0800

    R1 msg:提交1

---
+++ wind-fire-thunder-lightning/script/bili/bl.js
@@ -0,0 +1,15 @@
+
+var obj = JSON.parse($response.body);
+
+if(obj.data.ep_list != null){
+    for (let i = 0; i < obj.data.ep_list.length; i++) {
+        obj.data.ep_list[i].is_locked = false;
+        obj.data.ep_list[i].pay_gold = 0;
+        // obj.data.ep_list[i].is_in_free = true;
+        obj.data.ep_list[i].pay_mode = 0;
+    }
+}
+
+console.log("--------" + obj.data.ep_list[0].is_locked);
+
+$done({body : JSON.stringify(obj)});
