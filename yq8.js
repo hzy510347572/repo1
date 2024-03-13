fd5fb2a53964f41f7b144193d9a87e30615e3fb2
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 14:59:39 +0800

    Update yq8.js

--- yq8.js
+++ yq8.js
@@ -1,15 +1,15 @@
 
 var obj = JSON.parse($response.body);
 
-obj.body.totalPrice="0";
-obj.body.totalPriceDesc="0阅饼";
+// obj.body.totalPrice="0";
+// obj.body.totalPriceDesc="0阅饼";
 
-obj.body.payInfo.gapAmountAndroid=0;
-obj.body.payInfo.gapAmountDesc="0阅饼";
-obj.body.payInfo.price=0;
-obj.body.payInfo.totalPrice="0";
-obj.body.payInfo.gapAmountIOS="0";
-obj.body.payInfo.payDesc="0阅饼";
+obj.body.payInfo.gapAmountAndroid=99999;
+obj.body.payInfo.gapAmountDesc="99999阅饼";
+// obj.body.payInfo.price=0;
+//obj.body.payInfo.totalPrice="0";
+obj.body.payInfo.gapAmountIOS="999999";
+obj.body.payInfo.payDesc="999999阅饼";
 
 obj.body.userAccount.ableVoucherAmount="9999999";
 obj.body.userAccount.voucherAmountDesc="9999999代金券";
