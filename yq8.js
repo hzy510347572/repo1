d244798f2d1f61b07fee656ce84c5baaa00b1f68
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-13 15:02:58 +0800

    Update yq8.js

--- yq8.js
+++ yq8.js
@@ -4,12 +4,12 @@ var obj = JSON.parse($response.body);
 // obj.body.totalPrice="0";
 // obj.body.totalPriceDesc="0阅饼";
 
-obj.body.payInfo.gapAmountAndroid=99999;
-obj.body.payInfo.gapAmountDesc="99999阅饼";
+obj.body.payInfo.gapAmountAndroid=1;
+obj.body.payInfo.gapAmountDesc="1阅饼";
 // obj.body.payInfo.price=0;
 //obj.body.payInfo.totalPrice="0";
-obj.body.payInfo.gapAmountIOS="0";
-//obj.body.payInfo.payDesc="999999阅饼";
+obj.body.payInfo.gapAmountIOS="1";
+obj.body.payInfo.payDesc="1阅饼";
 
 obj.body.userAccount.ableVoucherAmount="9999999";
 obj.body.userAccount.voucherAmountDesc="9999999代金券";
