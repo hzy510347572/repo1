9bcdf93764cec9a79cf1520acdba6c27b67a7848
hzy <hzy510347572@163.com>
2024-03-14 12:35:01 +0800

    R1 msg:提交1

---
+++ wind-fire-thunder-lightning/script/blb/blb.js
@@ -0,0 +1,12 @@
+
+var obj = JSON.parse($response.body);
+
+obj.data.commonCounponCount = 99999999;
+obj.data.readCounponCount = 99999999;
+obj.data.counponCount = 99999999;
+// obj.data.useCounponType = 0;
+// obj.data.isUseCounpon = false;
+
+console.log(JSON.stringify(obj));
+
+$done({body : JSON.stringify(obj)});
---
+++ wind-fire-thunder-lightning/script/blb/blb1.js
@@ -0,0 +1,12 @@
+
+var obj = JSON.parse($response.body);
+
+obj.data.vipLevel = 4;
+obj.data.couponsRemain = 99999999;
+// obj.data.fireMoneyUsed = 0;
+obj.data.rmbCost = 9999;
+obj.data.fireMoneyRemain = 99999999;
+
+console.log(JSON.stringify(obj));
+
+$done({body : JSON.stringify(obj)});
