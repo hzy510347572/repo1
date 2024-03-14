52002c47abff4ac9729b89ddf768df82459158b2
hzy <hzy510347572@163.com>
2024-03-14 13:00:43 +0800

    R1 msg:提交1

--- wind-fire-thunder-lightning/script/blb/blb2.js
+++ wind-fire-thunder-lightning/script/blb/blb2.js
@@ -3,8 +3,8 @@ var obj = JSON.parse($response.body);
 
 if (obj.data != null) {
     for (let i = 0; i < obj.data.length; i++) {
-        obj.data[i].originNeedFireMoney = 0;
-        obj.data[i].needFireMoney = 0;
+        obj.data[i].expand.originNeedFireMoney = 0;
+        obj.data[i].expand.needFireMoney = 0;
         obj.data[i].isVip = false;
     }
 }
