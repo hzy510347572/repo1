360b0554cbf08853f72d0c1a5f7046ebdc26c633
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2024-03-16 04:06:46 +0800

    Update qd.js

--- wind-fire-thunder-lightning/script/qidian/qd.js
+++ wind-fire-thunder-lightning/script/qidian/qd.js
@@ -7,6 +7,17 @@ obj.Data.WordBalance=99999999;
 obj.Data.CouponAmount=99999999;
 obj.Data.CanUseWordBalance=1;
 obj.Data.CanFreeUnlock=1;
+obj.Data.CouponPrice=0;
+obj.Data.ReadingCouponsPrice=0;
+obj.Data.Price=0;
+obj.Data.CouponPointPrice=0;
+obj.Data.PriceInfo.DiscountPrice=0;
+obj.Data.PriceInfo.DiscountPointPrice=0;
+obj.Data.PriceInfo.OriginPrice=0;
+obj.Data.PriceInfo.OriginPointPrice=0;
+
+
+
 
 
 console.log(JSON.stringify(obj));
