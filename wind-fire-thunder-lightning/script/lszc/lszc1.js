7aff2aad09047034cd4cc1573f8e7570b6935a8d
hzy <hzy510347572@163.com>
2024-06-15 20:18:21 +0800

    R1 msg:提交2

--- wind-fire-thunder-lightning/script/lszc/lszc1.js
+++ wind-fire-thunder-lightning/script/lszc/lszc1.js
@@ -1,8 +1,11 @@
 
 var obj = JSON.parse($response.body);
 
-obj.locked=0;
+obj.locked=null;
 obj.code=0;
+obj.info="success";
+obj.uid=89052;
+obj.uuid="31a9ae2d-e047-474d-a3f1-ec214de8f172";
 
 console.log(JSON.stringify(obj));
 
