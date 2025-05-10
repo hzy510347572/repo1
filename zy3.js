a37620ce40484a45dd354973612984f1f4fc28ad
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2025-05-10 19:44:12 +0800

    Create zy3.js

---
+++ zy3.js
@@ -0,0 +1,20 @@
+
+var str1 = $response.body;
+
+var bodyStr = str1.substring(1, str1.length-1);
+
+bodyStr = bodyStr.replace("\\", "");
+
+console.log(JSON.stringify(bodyStr));
+
+var obj = JSON.parse(bodyStr);
+obj.body.accountInfo.totalAmount=9999999;
+obj.body.accountInfo.usrAmount=999999.90;
+obj.body.accountInfo.accountAmount=9999919;
+obj.body.accountInfo.couponAmount=80;
+
+console.log(JSON.stringify(obj));
+
+str1 = JSON.stringify(JSON.stringify(obj));
+
+$done({body : str1});
