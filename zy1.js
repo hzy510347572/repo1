7052d0c19e08ca8dbd7964da68a9c18fa3e9a833
hzy510347572 <59986279+hzy510347572@users.noreply.github.com>
2025-05-10 19:43:14 +0800

    Update zy1.js

--- zy1.js
+++ zy1.js
@@ -1,11 +1,13 @@
 
 var str1 = $response.body;
-str1 = str1.substring(1, str1.length-1);
-console.log(str1);
 
-var obj = eval("(" + str1 + ")");
-console.log(JSON.stringify(obj));
+var bodyStr = str1.substring(1, str1.length);
 
+bodyStr = bodyStr.replace("\\", "");
+
+console.log(JSON.stringify(bodyStr));
+
+var obj = JSON.parse(bodyStr);
 obj.body.accountInfo.totalAmount=9999999;
 obj.body.accountInfo.usrAmount=999999.90;
 obj.body.accountInfo.accountAmount=9999919;
@@ -14,6 +16,5 @@ obj.body.accountInfo.couponAmount=80;
 console.log(JSON.stringify(obj));
 
 str1 = JSON.stringify(JSON.stringify(obj));
-
 
 $done({body : str1});
