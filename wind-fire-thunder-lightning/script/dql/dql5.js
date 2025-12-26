595b410fb3ee88a910606de55dd45b94d04d2d15
qq510 <510347572@qq.com>
2025-12-26 12:32:24 +0800

    feat:dql4/5

--- wind-fire-thunder-lightning/script/dql/dql5.js
+++ wind-fire-thunder-lightning/script/dql/dql5.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 
-https:\/\/m\.mallcoo\.cn\/api\/gift\/giftmanager\//GetMallCard url script-response-body https://raw.githubusercontent.com/hzy510347572/repo1/refs/heads/branch1/wind-fire-thunder-lightning/script/dql/dql5.js
+https:\/\/m\.mallcoo\.cn\/api\/gift\/giftmanager\/GetMallCard url script-response-body https://raw.githubusercontent.com/hzy510347572/repo1/refs/heads/branch1/wind-fire-thunder-lightning/script/dql/dql5.js
 
 */
 
